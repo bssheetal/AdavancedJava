2ffe5c010a5d2d8b8e66ff1062e0f3b280dd8fce
sheetal <sheetal.srikumar@gmail.com>
2019-04-18 21:28:44 -0700

    saving changes

--- src/HashMap/RemoveDuplicates.java
+++ src/HashMap/RemoveDuplicates.java
@@ -5,18 +5,24 @@ import java.util.Iterator;
 
 public class RemoveDuplicates {
 
-    public void removesuplicatesfunc(int[] numbers)
+    public int removesuplicatesfunc(int[] numbers)
     {
         HashSet<Integer> removeduplicates=new HashSet<Integer>();
-        HashSet<Integer> newset=new HashSet<Integer>();
+        int idx = 0;
         for(int i=0;i<numbers.length;i++)
         {
             removeduplicates.add(numbers[i]);
+
         }
 
         Iterator<Integer>itr=removeduplicates.iterator();
         while (itr.hasNext())
+        {
             System.out.println(itr.next());
+            numbers[idx++]=itr.next();
+
+        }
+     return idx;
     }
 
 
