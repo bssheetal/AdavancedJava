d306b99e0ba7628dfa1a9c999f5ff00160e34c08
sheetal <sheetal.srikumar@gmail.com>
2019-04-20 21:59:22 -0700

    saving changes

--- src/String/ReverseWordsSentence.java
+++ src/String/ReverseWordsSentence.java
@@ -4,21 +4,20 @@ public class ReverseWordsSentence {
 
     public static void main(String args[])
     {
-//        String sentence="Let's take LeetCode contest";
-//        ReverseWordsSentence callmethod=new ReverseWordsSentence();
-//        //System.out.println(callmethod.reverseWords(sentence));
-//        ReverseInteger callreverseinteger=new ReverseInteger();
-//        long number=2147483647;
-//        //System.out.println(callreverseinteger.reverse(number));
-//        String Jewel = "aA";
-//        String Stone = "zxer";
-//        JewelsandStones findcount=new JewelsandStones();
-//        System.out.println(findcount.numJewelsInStones(Jewel,Stone));
-//        LengthofLastWord findlength=new LengthofLastWord();
-//        System.out.println(findlength.lengthOfLastWord("Helloo"));
         String sentence="Thi3s looks65 awesom4e!";
-//        StringClean st=new StringClean();
-//        st.cleanstring(sentence);
+        ReverseWordsSentence callmethod=new ReverseWordsSentence();
+        System.out.println(callmethod.reverseWords(sentence));
+        ReverseInteger callreverseinteger=new ReverseInteger();
+        long number=2147483647;
+        System.out.println(callreverseinteger.reverse(number));
+        String Jewel = "aA";
+        String Stone = "zxer";
+        JewelsandStones findcount=new JewelsandStones();
+        System.out.println(findcount.numJewelsInStones(Jewel,Stone));
+        LengthofLastWord findlength=new LengthofLastWord();
+        System.out.println(findlength.lengthOfLastWord("Helloo"));
+        StringClean st=new StringClean();
+       st.cleanstring(sentence);
         Stringcleanthoughtporcessway dd=new Stringcleanthoughtporcessway();
         dd.rmspcl(sentence);
 
