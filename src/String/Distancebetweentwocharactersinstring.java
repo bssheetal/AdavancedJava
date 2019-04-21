af40f2ed7cd9621b7b00d01554605c869648a097
sheetal <sheetal.srikumar@gmail.com>
2019-04-21 14:13:14 -0700

    find distance between strings

--- src/String/Distancebetweentwocharactersinstring.java
+++ src/String/Distancebetweentwocharactersinstring.java
@@ -1,4 +1,38 @@
 package String;
 
 public class Distancebetweentwocharactersinstring {
+
+    public void finddistance()
+    {
+        //find distance between L and t
+        //Get character L-0 and t-3
+        //
+        String word="Leet";
+        int s1=0;
+        int s2=0;
+        int distance=0;
+        for(int i=0;i<word.length();i++)
+    {
+        if((word.charAt(i)=='L'))
+        {
+            s1=i;
+        }
+        if((word.charAt(i)=='t'))
+        {
+            s2=i;
+        }
+    }
+
+        if(s2>s1)
+        {
+             distance=s2-s1;
+        }
+
+        else{
+            distance=s1-s2;
+        }
+
+        System.out.println(distance);
+    }
+
 }
--- src/String/ReverseWordsSentence.java
+++ src/String/ReverseWordsSentence.java
@@ -4,6 +4,8 @@ public class ReverseWordsSentence {
 
     public static void main(String args[])
     {
+        Distancebetweentwocharactersinstring fdistance=new Distancebetweentwocharactersinstring();
+        fdistance.finddistance();
         Putspclcharacterinbetween spcl=new Putspclcharacterinbetween();
         spcl.putspecialcharacter();
         String sentence="Thi3s looks65 awesom4e!";
